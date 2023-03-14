015548a810c19ce490bbfba4ea125a395a6065e8
Elysia <979130386@qq.com>
2023-03-14 10:28:22 +0800

    逐顶点着色

--- softRenderer/main.cpp
+++ softRenderer/main.cpp
@@ -1,151 +1,97 @@
-#include "core.h"
+#include <vector>
+#include <iostream>
 
-const TGAColor White = TGAColor(255, 255, 255, 255);
-const TGAColor Red = TGAColor(255, 0, 0, 255);
-const TGAColor RandColor = TGAColor(rand() % 255, rand() % 255, rand() % 255, 255);
+#include "./.h/model.h"
+#include "./shader/shader.h"
+#include "./.h/geometry.h"
+#include "./.h/tgaimage.h"
 
 //the resolution of output of the picture
-const int Width = 1920;
-const int Height = 1080;
-const int Depth = 255;
-TGAImage Image(Width, Height, TGAImage::RGB);
-std::vector<std::vector<int>> ZBuffer(Height + 1, std::vector<int>(Width + 1, std::numeric_limits<int>::min()));
-Vec3f LightDir(0, 0, -1);
-Vec3f camera(0, 0, 3);
+constexpr int g_Width = 800;
+constexpr int g_Height = 800;
 
-//model data
-Model* ModelBody = nullptr;
-Model* ModelFace = nullptr;
-Model* ModelHair = nullptr;
-Model* ModelCloak = nullptr;
+Model* g_model = nullptr;
+
+Vec3f g_LightDir { 0, -1, 1 };
+Vec3f g_Camera{ 2, 1, 3 };
+Vec3f g_Target{0, 0, 0};
+Vec3f g_Up{ 0,1,0 };
+Matrix g_ModelView; 
+Matrix g_Projection;
+Matrix g_Viewport;
+
+struct NormalMappingShader : public IShader
+{
+    //const Model& m_model;
+    Vec3f m_varyingIntensity; // written by vertex shader, read by fragment shader
+    mat < 2, 3, float > m_varyingUV;
+    mat<4, 4, float> m_uniformM;
+    mat<4, 4, float> m_uniformMIT;
+
+    /*
+    GouraudShader(const Model& m) : m_model(m) 
+    {
+        uniformL = proj<3>((g_ModelView * embed<4>(g_LightDir, 0.))).normalized(); // transform the light vector to view coordinates
+    }
+    */
+
+    virtual Vec4f Vertex(int iFace, int nthVert) 
+    {
+        m_varyingUV.set_col(nthVert, g_model->uv(iFace, nthVert));
+        m_varyingIntensity[nthVert] = std::max(0.f, g_model->normal(iFace, nthVert) * g_LightDir); // get diffuse lighting intensity
+        Vec4f m_vertex = embed<4>(g_model->vert(iFace, nthVert)); // read the vertex from .obj file
+        return g_Viewport * g_Projection * g_ModelView * m_vertex; // transform it to screen coordinates
+    }
+
+    virtual bool fragment(Vec3f bar, TGAColor& color) 
+    {
+        Vec2f uv = m_varyingUV * bar;
+        auto n = proj<3>(m_uniformMIT * embed<4>(g_model->normal(uv))).normalize();
+        auto l = proj<3>(m_uniformM * embed<4>(g_LightDir)).normalize();
+        auto intensity = std::max(0.f, n * l);
+        color = g_model->diffuse(uv) * intensity;
+        return false;                              // whether discard the pixel.true : yes, false : no;
+    }
+};
 
 int main(int argc, char** argv)
 {
-    if (2 == argc)
-    {
-        ModelBody = new Model(argv[1]);
-        ModelFace = new Model(argv[1]);
+    if (2 == argc) {
+        g_model = new Model(argv[1]);
     }
-    else
-    {
-        ModelBody = new Model("obj/fuhuabody.obj");
-        ModelFace = new Model("obj/fuhuaface.obj");
-        ModelHair = new Model("obj/fuhuahair.obj");
-        ModelCloak = new Model("obj/fuhuacloak.obj");
+    else {
+        g_model = new Model("obj/african_head.obj");
     }
-    
-    Matrix project = Matrix::identity(4);
-    Matrix viewPort = ViewPort(Width / 8, Height / 4, Width * 3 / 4, Height * 3 / 4, Depth);
-    project[3][2] = -1.f / camera.z;
+
+    TGAImage ZBuffer(g_Width, g_Height, TGAImage::GRAYSCALE);
+    TGAImage frameBuffer(g_Width, g_Height, TGAImage::RGB); // the output image
+    g_LightDir.normalize();
+
+    LookAt(g_Camera, g_Target, g_Up);
+    Projection( -1.f / (g_Camera - g_Target).norm());
+    ViewPort(g_Width / 8, g_Height / 8, g_Width * 3 / 4, g_Height * 3 / 4);
 
     //vert f x/y/z x/y/z x/y/z : x = vertex coord, y = uv coor, z = normal coor
     //face : triangle face
-    for (int i = 0; i < ModelBody->nfaces(); ++i)
+    NormalMappingShader shader;
+    shader.m_uniformM = g_Projection * g_ModelView;
+    shader.m_uniformMIT = (g_Projection * g_ModelView).invert_transpose();
+    for (int i = 0; i < g_model->nfaces(); ++i)
     {
-        std::vector<int> face = ModelBody->face(i);
-        Vec3f screenCoords[3];
-        Vec3f worldCoords[3];
-
-        for (int j = 0; j < 3; ++j)
+        Vec4f clipVerts[3];  //clip coordinates.written by VS, read by FS
+        for (int j : {0, 1, 2})
         {
-            Vec3f v = ModelBody->vert(face[j]);
-            screenCoords[j] = WorldToScreen(ModelBody->vert(face[j]), Width, Height);
-            worldCoords[j] = v;
+            clipVerts[j] = shader.Vertex(i, j);
         }
-
-        Vec3f n = (worldCoords[2] - worldCoords[0]) ^ (worldCoords[1] - worldCoords[0]);
-        n.normalize();
-        float intensity = n * LightDir;
-        if (intensity > 0)
-        {
-            Vec2i uv[3];
-            for (int k = 0; k < 3; ++k)
-                uv[k] = ModelBody->uv(i, k);
-            DrawTriangleZBuffer(screenCoords, uv, ZBuffer, Width, intensity, Image, ModelBody);
-        }
+        rasterization(clipVerts, shader, frameBuffer, ZBuffer);
     }
 
-    for (int i = 0; i < ModelFace->nfaces(); ++i)
-    {
-        std::vector<int> face = ModelFace->face(i);
-        Vec3f screenCoords[3];
-        Vec3f worldCoords[3];
+    //Image.flip_vertically();
+    frameBuffer.flip_vertically();
+    frameBuffer.write_tga_file("output.tga");
+    ZBuffer.flip_vertically();
+    ZBuffer.write_tga_file("zbuffer.tga");
 
-        for (int j = 0; j < 3; ++j)
-        {
-            Vec3f v = ModelFace->vert(face[j]);
-            screenCoords[j] = WorldToScreen(ModelFace->vert(face[j]), Width, Height);
-            worldCoords[j] = v;
-        }
-
-        Vec3f n = (worldCoords[2] - worldCoords[0]) ^ (worldCoords[1] - worldCoords[0]);
-        n.normalize();
-        float intensity = n * LightDir;
-        if (intensity > 0)
-        {
-            Vec2i uv[3];
-            for (int k = 0; k < 3; ++k)
-                uv[k] = ModelFace->uv(i, k);
-            DrawTriangleZBuffer(screenCoords, uv, ZBuffer, Width, intensity, Image, ModelFace);
-        }
-    }
-
-    for (int i = 0; i < ModelHair->nfaces(); ++i)
-    {
-        std::vector<int> face = ModelHair->face(i);
-        Vec3f screenCoords[3];
-        Vec3f worldCoords[3];
-
-        for (int j = 0; j < 3; ++j)
-        {
-            Vec3f v = ModelHair->vert(face[j]);
-            screenCoords[j] = WorldToScreen(ModelHair->vert(face[j]), Width, Height);
-            worldCoords[j] = v;
-        }
-
-        Vec3f n = (worldCoords[2] - worldCoords[0]) ^ (worldCoords[1] - worldCoords[0]);
-        n.normalize();
-        float intensity = n * LightDir;
-        if (intensity > 0)
-        {
-            Vec2i uv[3];
-            for (int k = 0; k < 3; ++k)
-                uv[k] = ModelHair->uv(i, k);
-            DrawTriangleZBuffer(screenCoords, uv, ZBuffer, Width, intensity, Image, ModelHair);
-        }
-    }
-
-    for (int i = 0; i < ModelCloak->nfaces(); ++i)
-    {
-        std::vector<int> face = ModelCloak->face(i);
-        Vec3f screenCoords[3];
-        Vec3f worldCoords[3];
-
-        for (int j = 0; j < 3; ++j)
-        {
-            Vec3f v = ModelCloak->vert(face[j]);
-            screenCoords[j] = WorldToScreen(ModelCloak->vert(face[j]), Width, Height);
-            worldCoords[j] = v;
-        }
-
-        Vec3f n = (worldCoords[2] - worldCoords[0]) ^ (worldCoords[1] - worldCoords[0]);
-        n.normalize();
-        float intensity = n * LightDir;
-        if (intensity > 0)
-        {
-            Vec2i uv[3];
-            for (int k = 0; k < 3; ++k)
-                uv[k] = ModelCloak->uv(i, k);
-            DrawTriangleZBuffer(screenCoords, uv, ZBuffer, Width, intensity, Image, ModelCloak);
-        }
-    }
-
-    Image.flip_vertically();
-    Image.write_tga_file("output.tga");
-
-    delete ModelBody;
-    delete ModelFace;
-    delete ModelHair;
-    delete ModelCloak;
+    delete g_model;
     return 0;
 }
